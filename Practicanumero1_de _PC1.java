c4bfc45f78e82a22148f2023eb4610248b2d037a
clau1176 <arrillagaclaudio@gmail.com>
2025-08-15 13:29:28 -0500

    Update Practicanumero1_de _PC1.java

--- Practicanumero1_de _PC1.java
+++ Practicanumero1_de _PC1.java
@@ -2,3 +2,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
+public class PracticaIPC1 {
+
+    //clase personaje 
+    static class personaje{
+        private static int contadorid = 1;
+        private int id;
+        
+    }
+}
