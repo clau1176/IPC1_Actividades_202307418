3fa0fb70f7f9019f5b549afd0c33261bd95bd4a2
clau1176 <arrillagaclaudio@gmail.com>
2025-12-07 16:58:33 -0600

    Add files via upload

---
+++ IPC1/src/TetrisConsola.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class TetrisConsola {
+    
+    // CONSTANTES DEL JUEGO
+    static final int FILAS = 20;
+    static final int COLUMNAS = 10;
+    static final int CARNE = 1234567; // Cambia este valor por tu carné
+    
