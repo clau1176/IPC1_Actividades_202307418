2b82f2e99690382bcd81df470d0142d01cdf1815
clau1176 <arrillagaclaudio@gmail.com>
2025-12-07 17:03:16 -0600

    Update TetrisConsola.java

--- IPC1/src/TetrisConsola.java
+++ IPC1/src/TetrisConsola.java
@@ -6,5 +6,6 @@ public class TetrisConsola {
     // CONSTANTES DEL JUEGO
     static final int FILAS = 20;
     static final int COLUMNAS = 10;
-    static final int CARNE = 1234567; // Cambia este valor por tu carné
+    static final int CARNE = 202307418; // Cambia este valor por tu carné
     
+
