35fe6b73a9fc760c9660c0d744c028807a810d66
clau1176 <arrillagaclaudio@gmail.com>
2026-08-03 17:34:10 -0600

    Update Tarea2.java 1

--- Tarea2/Tarea2.java
+++ Tarea2/Tarea2.java
@@ -1,1 +1,22 @@
-
+import java.util.Scanner;
+ 
+/**
+ * Tarea2 - IPC1
+ * Estudiante: Claudio Arrillaga
+ * Carné: 202307418
+ *
+ * Programa de consola que solicita, valida y procesa datos numéricos
+ * ingresados por el usuario, tanto en un arreglo unidimensional como
+ * en una matriz (arreglo bidimensional).
+ */
+public class Tarea2 {
+ 
+    // Scanner único para toda la entrada de consola del programa
+    static Scanner sc = new Scanner(System.in);
+ 
+    public static void main(String[] args) {
+ 
+     
+    }
+}
+ 
